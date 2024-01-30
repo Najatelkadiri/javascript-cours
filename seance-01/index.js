36b6ef9d3bf84d8de071e2226d765b477038e251
Najatelkadiri <156453019+Najatelkadiri@users.noreply.github.com>
2024-01-30 09:53:00 +0100

    first test

---
+++ seance-01/index.js
@@ -0,0 +1,32 @@
+console.log("hello javascript!!!")  //print//
+ function greet(nom) {
+    console.log("hello!" +nom)
+ }
+
+ greet("elfakiiri")
+
+
+ function somme(nombre1,nombre2){
+    return nombre1+nombre2
+ }
+console.log(somme(13,90))
+
+//declaration des variables
+const PI = 3.14
+function inscrire(){
+    let nom;
+    let prenom;
+    let age;
+    nom="ELKADIRI"
+    prenom="NAJAT";
+    age=21
+    if (age < 22){
+        var statut;
+        statut ="stagiaire"
+    }
+    console.log(nom)
+    console.log(statut)
+
+
+}
+
---
+++ seance-2/index.js
@@ -0,0 +1,16 @@
+let a=8;
+let b='8';
+if ( a===b){
+    console.log("okkkk")
+}else{
+    console.log( "les deux different")
+}
+let statut="etudiant";
+let age=20;
+
+   if(statut=="etudiant"  || age>20){
+    console.log("yees")
+
+   }
+
+
